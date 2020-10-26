10c1e5ba01f6e4a3841fbb100c607c0efc0e3f36
Sushma Kaluva <kaluvasushma@gmail.com>
2020-10-26 17:43:48 -0400

    added image links

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -133,7 +133,7 @@ function Portfolio() {
           </div>
           <div className="col-md-4">
             <div className="work-box">
-              <a href={stock} data-lightbox="gallery-aguadeluz">
+              <a href={stock1} data-lightbox="gallery-aguadeluz">
                 <div className="work-img">
                   <img src={stock1} alt="" className="img-fluid" />
                 </div>
@@ -203,7 +203,7 @@ function Portfolio() {
           </div>
           <div className="col-md-4">
             <div className="work-box">
-              <a href={stock} data-lightbox="gallery-todo">
+              <a href={stock2} data-lightbox="gallery-todo">
                 <div className="work-img">
                   <img src={stock2} alt="" className="img-fluid" />
                 </div>
@@ -273,7 +273,7 @@ function Portfolio() {
           </div>
           <div className="col-md-4">
             <div className="work-box">
-              <a href={stock} data-lightbox="gallery-medlingos">
+              <a href={stock3} data-lightbox="gallery-medlingos">
                 <div className="work-img">
                   <img src={stock3
                   } alt="" className="img-fluid" />
@@ -343,7 +343,7 @@ function Portfolio() {
           </div>
           <div className="col-md-4">
             <div className="work-box">
-              <a href={stock} data-lightbox="gallery-smth">
+              <a href={stock4} data-lightbox="gallery-smth">
                 <div className="work-img">
                   <img src={stock4} alt="" className="img-fluid" />
                 </div>
@@ -410,7 +410,7 @@ function Portfolio() {
           </div>
           <div className="col-md-4">
             <div className="work-box">
-              <a href={stock} data-lightbox="gallery-mf">
+              <a href={stock5} data-lightbox="gallery-mf">
                 <div className="work-img">
                   <img src={stock5} alt="" className="img-fluid" />
                 </div>
